4aa7bd67ca285b3913715b8796ce9c8de1723d85
FCornaire <fc.cornaire@etudiant.univ-lille1.fr>
2018-02-12 01:42:32 +0100

    re init

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 Ext.application({
-    name: 'SenchaCoin',
+  name: 'MyApp',
 
-    extend: 'SenchaCoin.Application',
+  requires: ['Ext.MessageBox'],
 
-    requires: [
-        'SenchaCoin.*'
-    ],
-
-    // The name of the initial view to create. With the classic toolkit this class
-    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
-    // modern toolkit, the main view will be added to the Viewport.
-    //
-    mainView: 'SenchaCoin.view.main.Main'
+  launch: function() {
+    Ext.Msg.alert('Hello Ext JS', 'Hello! Welcome to Ext JS.');
+  },
 });
